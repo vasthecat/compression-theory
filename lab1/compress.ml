efd69fa8e5fff6045e0ce141d8fd58b67cc5b655
Andrew Guschin <guschin.drew@gmail.com>
2022-09-16 17:57:07 +0400

    Добавлен интерфейс командной строки

--- lab1/compress.ml
+++ lab1/compress.ml
@@ -9,11 +9,32 @@ let write_file filename data =
     Array.iter (Out_channel.output_char chan) data;
     Out_channel.close chan;;
 
-let data = read_file "../texts/Тест_6.txt";;
-let archive = Huffman.compress data;;
-write_file "hello.gsch1" archive;;
+let usage_msg = "gsch1 -i -o [-compress] [-decompress]"
 
-let archive' = read_file "hello.gsch1";;
-let data' = Huffman.decompress archive';;
-write_file "decompressed.txt" data';;
+let is_compress = ref true
+let input_file = ref ""
+let output_file = ref ""
+
+let speclist = Arg.align ~limit:30 [
+    ("-i", Arg.Set_string input_file, " Установить файл для ввода");
+    ("-o", Arg.Set_string output_file, " Установить файл для вывода");
+    ("-compress", Arg.Set is_compress, " Режим запаковки");
+    ("-decompress", Arg.Clear is_compress, " Режим распаковки");
+];;
+
+Arg.parse speclist (fun _ -> ()) usage_msg;;
+
+if !input_file == "" || !output_file == "" then
+    Arg.usage speclist usage_msg
+else begin
+    if !is_compress then
+        let data = read_file !input_file in
+        let archive = Huffman.compress data in
+        write_file !output_file archive;
+    else begin
+        let archive' = read_file !input_file in
+        let data' = Huffman.decompress archive' in
+        write_file !output_file data';
+    end
+end
 
