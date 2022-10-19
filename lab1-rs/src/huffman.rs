453e4715cdc062d4b6d42092d60030d3bb5bdc30
Andrew Guschin <guschin.drew@gmail.com>
2022-10-19 20:04:04 +0400

    Исправлены ошибки в архиваторе и добавлен разархиватор

--- lab1-rs/src/huffman.rs
+++ lab1-rs/src/huffman.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use crate::priority_queue::PriorityQueue;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Bit { Zero, One, }
 impl Bit {
     fn from_u8(x: u8) -> Bit {
@@ -17,6 +17,7 @@ impl Bit {
     }
 }
 
+#[derive(Debug)]
 enum HuffmanTree<T> where T: std::cmp::Eq + std::hash::Hash + Copy {
     Leaf(T),
     Node(Box<HuffmanTree<T>>, Box<HuffmanTree<T>>),
@@ -42,13 +43,14 @@ impl<T: std::cmp::Eq + std::hash::Hash + Copy> HuffmanTree<T> {
         match tree {
             HuffmanTree::Leaf(val) => {
                 code.insert(*val, acc.clone());
-                acc.pop();
             },
             HuffmanTree::Node(left, right) => {
                 acc.push(Bit::Zero);
                 acc = HuffmanTree::dfs(left, acc, code);
+                acc.pop();
                 acc.push(Bit::One);
                 acc = HuffmanTree::dfs(right, acc, code);
+                acc.pop();
             },
         };
         return acc;
@@ -65,13 +67,6 @@ impl<T: std::cmp::Eq + std::hash::Hash + Copy> HuffmanTree<T> {
     }
 }
 
-struct Metadata {
-    weights: HashMap<u8, u32>,
-    tree: HuffmanTree<u8>,
-    code: HashMap<u8, Vec<Bit>>,
-    remainder: u8,
-}
-
 fn get_weights(data: &Vec<u8>) -> HashMap<u8, u32> {
     let mut freq = HashMap::new();
     data.iter().for_each(|byte| {
@@ -84,6 +79,14 @@ fn get_weights(data: &Vec<u8>) -> HashMap<u8, u32> {
         weights.insert(*byte, i as u32);
     });
     return weights;
+}
+
+#[derive(Debug)]
+struct Metadata {
+    weights: HashMap<u8, u32>,
+    tree: HuffmanTree<u8>,
+    code: HashMap<u8, Vec<Bit>>,
+    remainder: u8,
 }
 
 impl Metadata {
@@ -104,6 +107,29 @@ impl Metadata {
         };
     }
 
+    fn load(data: &Vec<u8>) -> Self {
+        let remainder = data[0];
+        let dict_len = data[1] as usize;
+        let mut weights = HashMap::new();
+        for i in 0..dict_len {
+            weights.insert(data[2 + 2 * i], data[2 + 2 * i + 1] as u32);
+        }
+
+        let mut queue = PriorityQueue::new();
+        for (value, priority) in weights.iter() {
+            queue.insert(*priority, *value);
+        }
+        let tree = HuffmanTree::from_queue(queue);
+        let code = tree.get_code();
+
+        return Self {
+            weights,
+            tree,
+            code,
+            remainder,
+        };
+    }
+
     fn dump(&self) -> Vec<u8> {
         let mut result = Vec::new();
 
@@ -118,15 +144,15 @@ impl Metadata {
     }
 }
 
-struct Compressor {
+struct BitWriter {
     buffer: Vec<Bit>,
     remainder: u8,
     result: Vec<u8>,
 }
 
-impl Compressor {
+impl BitWriter {
     fn new() -> Self {
-        Compressor {
+        Self {
             buffer: Vec::new(),
             remainder: 0,
             result: Vec::new(),
@@ -167,19 +193,84 @@ pub fn compress(data: &Vec<u8>) -> Vec<u8> {
     let mut result = Vec::new();
     let mut metadata = Metadata::compute(data);
 
-    let mut compressor = Compressor::new();
+    let mut writer = BitWriter::new();
     data.iter()
         .for_each(|byte| {
             let bits = metadata.code.get(byte).unwrap();
-            compressor.write_bits(&bits);
+            writer.write_bits(&bits);
          });
-    compressor.finish();
-    metadata.remainder = compressor.remainder;
+    writer.finish();
+    metadata.remainder = writer.remainder;
 
     let md_dump = metadata.dump();
     md_dump.iter().for_each(|byte| result.push(*byte));
-    compressor.result.iter().for_each(|byte| result.push(*byte));
+    writer.result.iter().for_each(|byte| result.push(*byte));
 
     return result;
 }
 
+struct BitReader<'a> {
+    data: &'a Vec<u8>,
+    metadata: &'a Metadata,
+    buffer: Vec<Bit>,
+    ptr: usize,
+}
+
+impl<'a> BitReader<'a> {
+    fn new(data: &'a Vec<u8>, metadata: &'a Metadata) -> Self {
+        Self {
+            data,
+            metadata,
+            buffer: Vec::new(),
+            ptr: 0,
+        }
+    }
+
+    fn read_byte(&mut self) {
+        if let Some(byte) = self.data.get(self.ptr) {
+            for i in 0..=7 {
+                let bit = byte & (1 << i);
+                self.buffer.push(Bit::from_u8(bit));
+            }
+            if self.ptr == self.data.len() - 1 {
+                for _ in 0..self.metadata.remainder {
+                    self.buffer.pop();
+                }
+            }
+            self.buffer.reverse();
+            self.ptr += 1;
+        }
+    }
+
+    fn read_bit(&mut self) -> Option<Bit> {
+        if self.buffer.len() == 0 {
+            self.read_byte();
+        }
+        self.buffer.pop()
+    }
+}
+
+pub fn decompress(archive: &Vec<u8>) -> Vec<u8> {
+    let mut result = Vec::new();
+    let metadata = Metadata::load(archive);
+
+    let data = archive[2 + metadata.weights.len() * 2..].to_vec();
+    let mut reader = BitReader::new(&data, &metadata);
+
+    let mut state = &metadata.tree;
+    while let Some(bit) = reader.read_bit() {
+        if let HuffmanTree::Node(left, right) = state {
+            match bit {
+                Bit::Zero => state = left,
+                Bit::One => state = right,
+            }
+        }
+
+        if let HuffmanTree::Leaf(byte) = state {
+            result.push(*byte);
+            state = &metadata.tree;
+        }
+    }
+
+    return result;
+}
--- lab1-rs/src/main.rs
+++ lab1-rs/src/main.rs
@@ -1,11 +1,16 @@
 mod huffman;
 mod priority_queue;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let data = vec![1, 2, 3, 4];
     let archive = huffman::compress(&data);
-
-    std::fs::write("hi.txt", &data);
-    std::fs::write("hello.gsch1", &archive);
     println!("{:?}", archive);
+
+    // std::fs::write("hi.txt", &data)?;
+    // std::fs::write("hello.gsch1", &archive)?;
+
+    let new_data = huffman::decompress(&archive);
+    println!("{:?}", new_data);
+
+    return Ok(());
 }
